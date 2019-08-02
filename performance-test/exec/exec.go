1f625293f556f3f5379babf5cd517338c2258dde
Michael Zhao <michael.zhao@arm.com>
2019-08-02 14:11:17 +0800

    x

--- performance-test/exec/exec.go
+++ performance-test/exec/exec.go
@@ -7,6 +7,18 @@ import (
 )
 
 func main() {
+	michaelf, mierr := os.Create("/go/src/github.com/docker/docker/trace.out")
+	if mierr != nil {
+		panic(mierr)
+	}
+	defer michaelf.Close()
+
+	mierr = trace.Start(michaelf)
+	if mierr != nil {
+		panic(mierr)
+	}
+	defer trace.Stop()
+
 	logrus.SetLevel(logrus.DebugLevel)
 	logrus.SetFormatter(&logrus.TextFormatter{
 		TimestampFormat: jsonmessage.RFC3339NanoFixed,
